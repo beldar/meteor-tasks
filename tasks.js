c25ba3d6083dfc88e8236a01786cea1fb93f6d7b
Beldar <beldar.cat@gmail.com>
2016-03-31 21:58:57 +0100

    Finishing up

--- tasks.js
+++ tasks.js
@@ -1,30 +1,32 @@
-Tasks = new Mongo.Collection('tasks');
+const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
 
+  Session.set('filter', {});
+
   Template.tasks.helpers({
-    tasks: function () {
-      return Tasks.find({});
+    tasks() {
+      return Tasks.find(Session.get('filter'));
     }
   });
 
   Template.task.helpers({
-    isMine: function() {
+    isMine() {
       return this.assigned && Meteor.userId() === this.assigned._id;
     },
-    isAvailable: function() {
+    isAvailable() {
       return !this.assigned;
     },
-    isSelected: function(value) {
+    isSelected(value) {
       return this.status === value ? 'selected' : null
     }
   });
 
   Template.task.events({
-    'click .tasks__task__claim-button': function (event, template) {
+    'click .tasks__task__claim-button'() {
       Tasks.update(this._id, {
         $set: {
           assigned: Meteor.user(),
@@ -33,8 +35,8 @@ if (Meteor.isClient) {
       });
     },
 
-    'change .tasks__task__status-select': function (event, template) {
-      var updateObj = {
+    'change .tasks__task__status-select'() {
+      let updateObj = {
         status: event.target.value
       };
 
@@ -47,7 +49,7 @@ if (Meteor.isClient) {
       });
     },
 
-    'click .tasks__task__release-button': function (event, template) {
+    'click .tasks__task__release-button'(){
       Tasks.update(this._id, {
         $set: {
           assigned: false,
@@ -56,22 +58,60 @@ if (Meteor.isClient) {
       });
     },
 
-    'click .debug': function (event, template) {
+    'click small'(){
+      Tasks.remove(this._id);
+    },
+
+    'click .debug'() {
       console.log(this);
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    //Tasks.remove({});
-    Tasks.insert({
-      name: 'A task',
-      description: 'A description of the task',
-      assigned: false,
-      createdAt: new Date(),
-      finishedAt: false,
-      status: 'unclaimed'
-    })
+  Template.add.events({
+    'click .task__add__button'(e) {
+      e.preventDefault();
+      let overlay = document.querySelector('.overlay');
+      overlay.classList.add('show');
+    },
+    'click .task__add__cancel'(e) {
+      e.preventDefault();
+      let overlay = document.querySelector('.overlay');
+      overlay.classList.remove('show');
+    },
+    'submit .task__add__form'(e) {
+      e.preventDefault();
+      const form = e.target;
+      const title = form.title.value;
+      const desc = form.description.value;
+      const tags = form.tags.value.split(',');
+
+      if (title === '' || desc === '' || tags === '') {
+        return alert('All fields must have a value!')
+      }
+
+      Tasks.insert({
+        name: title,
+        description: desc,
+        tags: tags,
+        assigned: false,
+        createdAt: new Date(),
+        finishedAt: false,
+        status: 'unclaimed'
+      });
+      let overlay = document.querySelector('.overlay');
+      overlay.classList.remove('show');
+    }
   });
+
+  Template.filter.events({
+    'change .tasks__filter'(e) {
+      let val = e.target.value;
+
+      if (val === '-1') {
+        Session.set('filter', {});
+      } else {
+        Session.set('filter', {status: val});
+      }
+    }
+  })
 }
